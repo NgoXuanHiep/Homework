6d180c5e04a4462cd7e5821d6eaa79d4f711c29c
NgoXuanHiep <hiepxuan07@gmail.com>
2024-10-31 20:48:33 +0700

    BaiLab

---
+++ kiemtrasonguyen.java
@@ -0,0 +1,11 @@
+
+package NgoXuanHiep_29219047048;
+import java.util.Scanner;
+public class kiemtrasonguyen {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so: "); 
+        System.out.println(scanner.hasNextInt());
+        scanner.close();
+     }
+}
